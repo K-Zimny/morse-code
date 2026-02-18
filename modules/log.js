8c9c399d66a60080dc7a1c44d2b305d2849def68
Ken Zimny <kenneth.zimny@gmail.com>
2026-02-18 08:17:45 -0600

    Clean up pre launch

--- modules/log.js
+++ modules/log.js
@@ -10,11 +10,9 @@ function writeToLog(signal) {
   const el = document.createElement("li");
 
   if (signal === "short") {
-    console.log("short");
     el.classList.add("short");
   }
   if (signal === "long") {
-    console.log("long");
     el.classList.add("long");
   }
 
